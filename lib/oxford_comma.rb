5a00b51a3f43428dcce2f8cb97edd62ac532843e
Kunal <kdshah6593@gmail.com>
2020-11-10 16:20:29 -0500

    commit tree ebe0d39e4b9af8efb73921c45fcef8d3c514a4cb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        return array[0]
+    elsif
+        array.size == 2
+        return array.join(" and ")
+    else
+        arr_copy = array
+        arr_last = arr_copy.pop
+        newArr = arr_copy.join(", ") + ", and " + arr_last
+    end 
 end
